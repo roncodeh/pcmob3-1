42e3344f519605d84bdd2bad23660c6ae3ed5e19
roncodeh <104334134+roncodeh@users.noreply.github.com>
2022-07-01 22:45:42 +0800

    commit

--- App.js
+++ App.js
@@ -1,17 +1,47 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { CardStyleInterpolators, createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import BlockRGB from "./components/BlockRGB";
+import { FlatList, TouchableOpacity } from "react-native-web";
 
 function HomeScreen() {
- return (
-   <View style={styles.container}>
-     <Text>Open up App.js to start working on your app!</Text>
-     <StatusBar style="auto" />
-   </View>
- );
+  const [colorArray, setColorArray] = useState([
+  { red: 255, green: 0, blue: 0, id: "0" },
+  { red: 0, green: 255, blue: 0, id: "1" },
+  { red: 0, green: 0, blue: 255, id: "2" },
+  ]);
+  
+function renderItem({item}) {
+  return <BlockRGB red={item.red} green={item.green} blue={item.blue} />; 
 }
+
+function addColor(){
+  setColorArray([
+    ...colorArray,
+    {
+      red: Math.floor(Math.random() * 255),
+      green: Math.floor(Math.random() * 255),
+      blue: Math.floor(Math.random() * 255),
+      id: `${colorArray.length}`,
+    },
+  ]);
+}
+
+
+return (
+    <View style={styles.container}>
+      <TouchableOpacity 
+        style={{height: 40, justifyContent:"center"}}
+        onPress={addColor}
+        >
+        <Text style={{ color: "black" }}>Add Color</Text>
+      </TouchableOpacity>
+      <FlatList style={styles.list} data={colors} renderItem={renderItem} />
+    </View>
+  );
+  }
 
 const Stack = createStackNavigator();
 
